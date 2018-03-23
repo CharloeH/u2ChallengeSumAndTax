a9da77c920ba252b40e2bd5ebb5aa62589b6ae12
SebastianH11 <36234258+SebastianH11@users.noreply.github.com>
2018-03-23 15:09:30 -0400

    Add files via upload
    cool cool

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace u2ChallengeSumAndTaxSebastian
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void btnClick_Click(object sender, RoutedEventArgs e)
+        {
+            string Input = txtInput.Text;
+            int.TryParse(Input.Substring(0, Input.IndexOf('\r')), out int numOfLines);
+            for (int i = 0; i < numOfLines; ++i)
+            {
+                MessageBox.Show("10/10 ign would loop again");
+            }
+        }
+    }
+ }
+
